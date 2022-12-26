c7c43e10aa707926995107e6006e5b9da0e9009c
Thiago <thiago.spereira02@gmail.com>
2022-12-26 09:26:30 -0300

    setting eslint

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+	"env": {
+		"browser": true,
+		"es2021": true
+	},
+	"extends": [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"plugin:@typescript-eslint/recommended"
+	],
+	"overrides": [
+	],
+	"parser": "@typescript-eslint/parser",
+	"parserOptions": {
+		"ecmaVersion": "latest",
+		"sourceType": "module"
+	},
+	"plugins": [
+		"react",
+		"@typescript-eslint"
+	],
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		],
+		"import/no-unresolved": "off",
+	},
+	"settings": {
+		"import/resolver": {
+			"typescript": {}
+		}
+	}
+};
